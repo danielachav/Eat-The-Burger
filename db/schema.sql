978d3bf4a6c86c73a7e8ef55d2365de8aa897377
Daniela Chavez <danielachavez18@gmail.com>
2019-11-12 22:01:43 -0600

    added the code

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,11 @@
 CREATE DATABASE burgers_db;
+USE burgers_db;
 
-CREATE TABLE burgers;
+CREATE TABLE burgers
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
 
-id AUTO_INCREMENT
-burger_name STRING
-devoured BOOLEAN
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("bacon cheeseburger", false), ("cheeseburger", false), ("spicy jelapeno burger", false);
